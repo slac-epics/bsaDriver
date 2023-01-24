b60c644e20c586b4e52e68063e33d386f1489cae
Kukhee Kim <khkim@slac.stanford.edu>
2023-01-23 20:13:32 -0800

    implement own device support for BSA for performance improvement

---
+++ bsaDriverApp/src/devScBsa.c
@@ -0,0 +1,126 @@
+#include <stdint.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "alarm.h"
+#include "cvtTable.h"
+#include "dbDefs.h"
+#include "dbAccess.h"
+#include "dbScan.h"
+
+#include "recGbl.h"
+#include "recSup.h"
+#include "devSup.h"
+#include "link.h"
+#include "epicsExport.h"
+
+#include "waveformRecord.h"
+
+#include "devScBsa.h"
+
+
+
+typedef struct {
+    void       *pvt;
+    IOSCANPVT  ioscanpvt;
+} dev_pvt;
+
+
+void process_bsa_pv(devBsaPvt_t *p)
+{
+    dev_pvt *dpvt = (dev_pvt *) p->dpvt;
+    if(dpvt && dpvt->ioscanpvt) scanIoRequest(dpvt->ioscanpvt);
+}
+
+
+static long get_ioint_info(int cmd, dbCommon *prec, IOSCANPVT *ppvt)
+{
+    dev_pvt *dpvt = (dev_pvt *) prec->dpvt;
+
+    if(dpvt){
+        if(dpvt->ioscanpvt == 0) scanIoInit(&dpvt->ioscanpvt);
+        *ppvt = dpvt->ioscanpvt;
+    } else {
+        *ppvt = 0;
+    }
+
+
+    return 0;
+}
+
+
+
+static long init_record(waveformRecord *prec)
+{
+    char port[32], key[32], param[32];
+    int  edef;
+
+    devBsaPvt_t *ppvt;
+    dev_pvt *dpvt = (dev_pvt *) malloc(sizeof(dev_pvt));
+    memset(dpvt, 0, sizeof(dev_pvt));
+
+    switch(prec->inp.type) {
+        case INST_IO:
+            sscanf(prec->inp.value.instio.string, "%s %s %s %d", port, key, param, &edef);
+            ppvt = find_bsa_pvt(port, key, param, edef);
+            if(!ppvt) goto err;
+            dpvt->pvt = (void *) ppvt;
+            ppvt->dpvt = (void *) dpvt;
+            if(prec->bptr) { ppvt->save_bptr = prec->bptr; free(prec->bptr); }  // since we are trying zero copy buffer, we do not need pre-allocated memory in the record
+            break;
+        default:
+            err:
+            recGblRecordError(S_db_badField, (void *) prec,
+                              "devScBsa (init_record) Illegal INP field");
+            free(dpvt);
+            prec->udf = TRUE;
+            prec->dpvt = NULL;
+            return S_db_badField;
+    }
+
+    prec->udf = FALSE;
+    prec->dpvt = (void *) dpvt;
+    return 0;
+}
+
+static long read_wf(waveformRecord *prec)
+{
+    int nreq;
+    devBsaPvt_t *ppvt;
+    dev_pvt     *dpvt = (dev_pvt *) prec->dpvt;
+    if(!dpvt) return 0;
+
+    ppvt = dpvt->pvt;
+    prec->time = ppvt->ts;
+    
+    nreq = ppvt->nreq;
+    if(nreq > prec->nelm) nreq = prec->nelm;
+    if(nreq != prec->nord) prec->nord = nreq;
+    //memcpy(prec->bptr, ppvt->bptr, nreq * ppvt->entry_sz);
+    prec->bptr = ppvt->bptr;    // using zerro  copy buffer
+
+    
+
+    return 0;
+}
+
+
+struct {
+    long      number;
+    DEVSUPFUN report;
+    DEVSUPFUN init;
+    DEVSUPFUN init_record;
+    DEVSUPFUN get_ioint_info;
+    DEVSUPFUN read_wf;
+} devScBsa = {
+    5,
+    NULL,
+    NULL,
+    init_record,
+    get_ioint_info,
+    read_wf
+};
+epicsExportAddress(dset, devScBsa);
+
---
+++ bsaDriverApp/src/devScBsa.h
@@ -0,0 +1,33 @@
+#ifndef DEV_SCBSA_H
+#define DEV_SCBSA_H
+
+
+#define PARAM_BSAMEAN "BSAMEAN"
+#define PARAM_BSANUM  "BSANUM"
+#define PARAM_BSARMS2 "BSARMS2"
+#define PARAM_BSAPID  "BSAPID"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct {
+    void *dpvt;
+    epicsTimeStamp ts;
+    int  nreq;
+    int  entry_sz;
+    void *bptr;
+    void *save_bptr;
+} devBsaPvt_t;
+
+
+devBsaPvt_t * find_bsa_pvt(char *port, char *key, char *param, int edef);
+void process_bsa_pv(devBsaPvt_t *p);
+
+
+#ifdef __cplusplus
+}      /* extern C */
+#endif
+
+
+#endif /* DEV_SCBSA_H */
